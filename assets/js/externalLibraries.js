a0ca5126e65dc7aef771fada117ec99434e518d5
SRFeesha <srfeesha@gmail.com>
2019-03-03 10:34:25 +0100

    bubble animation, removed header

--- assets/js/externalLibraries.js
+++ assets/js/externalLibraries.js
@@ -1,14 +1,41 @@
+// card animation to open another page.
+function delay(URL) {
+  document.getElementsByClassName("circle")[0].classList.toggle("circle-expanded");
+  setTimeout(function () {
+    window.location = URL
+  }, 300);
+}
 
 $(document).ready(function () {
+  // Library for cool shading effect
   AOS.init();
 
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  var prevScrollpos = window.pageYOffset;
+
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementsByClassName("site-header")[0].style.top = "0";
+    } else {
+      document.getElementsByClassName("site-header")[0].style.top = "-61px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+  //hamburger menu
+  $('button').on('click', function () {
+    $('body').toggleClass('open');
+  });
+
+
   $('.responsive').slick({
-    arrows: true,
-    dots: true,
+    arrows:   true,
+    dots:     true,
     infinite: true,
-    speed: 300,
-    responsive: [
-      {
+    speed:    300,
+    // remove arrow on device because it breaks the viewport
+    responsive: [{
         breakpoint: 1024,
         settings: {
           arrows: true
@@ -28,25 +55,4 @@ $(document).ready(function () {
       }
     ]
   });
-  
-  
 });
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("site-header")[0].style.top = "0";
-  } else {
-    document.getElementsByClassName("site-header")[0].style.top = "-61px";
-  }
-  prevScrollpos = currentScrollPos;
-} 
-
-$('button').on('click', function(){
-  $('body').toggleClass('open');
-});
-
-
